7fdbdfa668ec0e94bbecab7c0cfae3d3d046c03c
ForgeGod <tommyrcao@gmail.com>
2023-10-24 20:22:25 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+          <h1>CS 230L</h1>
+          <h2>Section - 002</h2>
+          <p>WVU ID: 800358697</p>
+           <p>Hi I am Thomas Cao</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
