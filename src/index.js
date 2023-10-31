5df85e49f914cd837078bea6c78138e8fa07de99
ForgeGod <tommyrcao@gmail.com>
2023-10-30 22:08:47 -0400

    Lab 6 update

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function NavBar() {
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="#">Action</a></li>
                                 <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><hr class="dropdown-divider"></li>
+                                <li><hr class="dropdown-divider"></hr></li>
                                 <li><a class="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </li>
@@ -45,7 +45,7 @@ function NavBar() {
                         </li>
                     </ul>
                     <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
                             <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </div>
